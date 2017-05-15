f7effda9321f8ff59361fd27ec2ef08286955817
Stanisław Chmiela <sjchmiela@gmail.com>
2017-05-16 01:25:59 +0200

    Add events to the new story

--- generator/generator.rb
+++ generator/generator.rb
@@ -34,7 +34,8 @@ articles = [
   ),
 ]
 
-def profile_info(user)
+def profile_info(user, attributes_to_merge = {})
+  user = OpenStruct.new(user.to_h.merge(attributes_to_merge))
   {
     type: 'PROFILE_INFO',
     timestamp: Time.now.to_s,
@@ -73,14 +74,18 @@ puts [
   profile_info(arek),
   profile_info(basia),
   like(arek, articles[0]),
+  like(arek, articles[1]),
   profile_info(ewa),
   profile_info(daniel),
   profile_info(felicja),
+  like(felicja, articles[1]),
   profile_info(grzegorz),
   profile_info(helena),
-  like(ewa, articles[2]),
-  profile_info(igor),
-  profile_info(jennifer),
-  like(felicja, articles[1]),
   like(helena, articles[2]),
+  profile_info(igor),
+  like(igor, articles[2]),
+  profile_info(jennifer),
+  like(jennifer, articles[2]),
+  profile_info(helena, job: 'computer interfaces designer', tags: helena.tags + ['java'])
+  profile_info(daniel, city: 'Cracow')
 ].map { |hash| send(hash) }.to_json
